50a728ae65613828eb92ccda7570a3729ab9864a
Jakub Janata <jakubjanata@gmail.com>
2018-05-21 18:58:41 +0200

    fix basic auth

--- src/LiquidBee.Mailgun/MailgunClient.cs
+++ src/LiquidBee.Mailgun/MailgunClient.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Options;
 
@@ -16,8 +18,12 @@ namespace LiquidBee.Mailgun
 
         public async Task<HttpResponseMessage> Send(string endpoint, HttpContent content)
         {
-            return await new HttpClient()
-                .PostAsync(new Uri($"https://api:{_options.Key}@api.mailgun.net/v3/{_options.Domain}/{endpoint}"), content);
+            var client = new HttpClient();
+
+            var authString = Convert.ToBase64String(Encoding.ASCII.GetBytes($"api:{_options.Key}"));
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", authString);
+
+            return await client.PostAsync(new Uri($"https://api.mailgun.net/v3/{_options.Domain}/{endpoint}"), content);
         }
     }
 }
